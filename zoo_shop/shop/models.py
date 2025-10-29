962370c80d38b3fc5d4aa2548cdaf3e06660abe7
Artem <artem@MacBook-Air-Artem.local>
2025-10-29 12:21:17 -0500

    22.10.2025

--- zoo_shop/shop/models.py
+++ zoo_shop/shop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # cat, dog, bird, fish, reptile
+    brand = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2) #float
+    stock = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
